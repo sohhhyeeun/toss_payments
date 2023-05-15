1f2204aee2fb3bd241df991a9fb27ed9ea8e8af2
sohhhyeeun <syeankm12076@gmail.com>
2023-05-15 11:19:06 +0900

    키 변경

--- src/main/java/com/tosspayments/demo/payment/PaymentController.java
+++ src/main/java/com/tosspayments/demo/payment/PaymentController.java
@@ -26,7 +26,7 @@ public class PaymentController {
             @RequestParam(value = "amount") Integer amount,
             @RequestParam(value = "paymentKey") String paymentKey) throws Exception {
 
-        String secretKey = "test_ak_ZORzdMaqN3wQd5k6ygr5AkYXQGwy:";
+        String secretKey = "test_sk_GKNbdOvk5rkgvjvW2ozrn07xlzmj:";
 
         Base64.Encoder encoder = Base64.getEncoder();
         byte[] encodedBytes = encoder.encode(secretKey.getBytes("UTF-8"));
